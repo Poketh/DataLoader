e088261bb246e3f2bbab1b4328d51800a81dd3c7
OFRBG <fi@SaturnMx.local>
2018-05-01 23:03:16 -0500

    address check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Layout, Divider, Switch } from 'antd';
+import { Row, Col, Layout, Divider, Switch, Input, Tooltip } from 'antd';
 
 import MatrixDescriptor from './MatrixDescriptor.js';
 import './App.css';
@@ -10,12 +10,14 @@ import logo from './logo.png';
 import abi from './ERC891.json';
 
 const { Header, Footer, Content } = Layout;
+const Search = Input.Search;
+
 const itemList = Array(151).fill().map((x,i) => i+1);
 
 const unitX = 100;
 const unitY = 100;
 
-const address = '0xee473f5cd7e7a199a0ce1b2995a10ed0cdb9a8c1';
+const pokethAddress = '0xee473f5cd7e7a199a0ce1b2995a10ed0cdb9a8c1';
 const styles = {
   'blue': '#003049',
   'red': '#D62828',
@@ -28,20 +30,27 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {display: null, balances: [], displayBalance: '0' }
+    this.state = {display: null, balances: [], displayBalance: '0', addressLookup: '0x0' }
 
     this.showDetail = this.showDetail.bind(this);
+    this.loadBalanceFor = this.loadBalanceFor.bind(this);
   }
 
-  componentDidMount(){
-    var balances = window.web3.eth.contract(abi).at(address);
-
+  componentDidMount() {
     window.web3.eth.getAccounts((e,a) => {
-      balances.balanceOf.call([a[0]], (err, ans) => {
+      this.loadBalanceFor(a[0]);
+    });
+  }
+
+  loadBalanceFor(address) {
+    if(!/0x[a-f0-9]{40}/.test(address.toLowerCase())) address = window.web3.eth.accounts[0];
+
+    var balances = window.web3.eth.contract(abi).at(pokethAddress);
+    
+    balances.balanceOf.call([address], (err, ans) => {
         const balancesData = ans.map((v) => v.c[0])
         this.setState({balances: balancesData})
       });
-    });
   }
 
   showDetail(id, name,c, xo, yo){
@@ -59,7 +68,7 @@ class App extends Component {
           <Header style={{background: styles.white}}>
             <Row type='flex' justify='space-between'>
               <Col span={1}><img style={{height:'50px', width:'auto'}} src={logo}/></Col>
-              <Col span={10}><h4 style={{color:styles.blue}}>Loaded balances from {address}.</h4></Col>
+              <Col span={10}><h4 style={{color:styles.blue}}>Loaded balances from {pokethAddress}.</h4></Col>
               <Col span={6}>
                 <a href='#' style={{minWidth:'150px', padding:'10px', background: styles.orange, color: styles.blue}}>download miner</a>
               </Col>
@@ -67,6 +76,18 @@ class App extends Component {
               <Col span={1}></Col>
             </Row>
           </Header>
+          <Content style={{paddingTop:'32px', padding: '24px', background: styles.blue}}>
+            <Row>
+              <Col span={12}>
+                <h1 style={{color: styles.white}}>Load balance from </h1>
+                <Row>
+                  <Col offset={1}>
+                    <Search placeholder={'ETH Address'} onSearch={v => this.loadBalanceFor(v)} style={{width:'400px'}}/>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Content>
           <Content style={{paddingTop:'32px', padding: '24px', background: styles.blue}}>
             <Row type='flex' gutter={16} justify='space-around'>
               <Col span={12} offset={0} style={{display: 'inline-block'}}>
